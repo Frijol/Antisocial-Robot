b929b97c52a9fe822899e0b36ce4d05d013354df
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-18 18:32:49 -0800

    theoretically mvp

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var tessel = require('tessel');
-var car = require('servo-car').use(tessel.port['B'], function () {
-  car.forward();
-  setTimeout(function () {
-    car.left();
-    setTimeout(function () {
-      car.stop();
-    }, 3000);
-  }, 3000);
+require('tesselate') ({
+  modules: {
+    A: ['ambient-attx4', 'ambientL'],
+    C: ['ambient-attx4', 'ambientR']
+  },
+}, function(tessel, modules) {
+  var left = modules.ambientL;
+  var right = modules.ambientR;
+  var car = require('servo-car').use(tessel.port['B'], function () {
+    setInterval( function () {
+      right.getSoundLevel(function (err, rightNoise) {
+        left.getSoundLevel (function (err, leftNoise) {
+          console.log('Right:', rightNoise, '\tLeft:', leftNoise);
+          if (rightNoise > leftNoise) {
+            console.log('Go left');
+            car.left();
+          } else if (rightNoise < leftNoise) {
+            console.log('Go right');
+            car.right();
+          } else {
+            console.log('Plunge ahead');
+            car.forward();
+          }
+        });
+      });
+    }, 500);
+  });
 });
